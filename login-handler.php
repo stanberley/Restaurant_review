121eeaf20be88a6e10a75321538aa1a8ab6669e8
venus low <venuslow@venuss-MacBook-Air-2.local>
2026-03-17 20:27:51 +0800

    separate uth logic from UI pages

---
+++ login-handler.php
@@ -0,0 +1,15 @@
+<?php
+session_start();
+
+if (isset($_GET['email']) && isset($_GET['name']) && isset($_GET['role'])) {
+
+    $_SESSION['email'] = $_GET['email'];
+    $_SESSION['name'] = $_GET['name'];
+    $_SESSION['role'] = $_GET['role'];
+
+    header("Location: dashboard.php");
+    exit();
+}
+
+header("Location: login.php");
+exit();
